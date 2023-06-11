6f1d5593d489693bfb058394740eec643353c0d5
AnDr10wA <45951252+AnDr10wA@users.noreply.github.com>
2023-06-11 21:47:16 +0300

    Initial commit

---
+++ src/main/java/koschei/KoscheiTheDeathless.java
@@ -0,0 +1,25 @@
+package koschei;
+
+import koschei.models.Needle7;
+import koschei.models.Ocean1;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KoscheiTheDeathless {
+
+    private Ocean1 ocean;
+    private Needle7 needle;
+    public String getRulesByDeth() {
+        return "На свете есть океан , " + ocean.toString() + needle.toString();
+    }
+
+    @Autowired
+    public void setOcean(Ocean1 ocean) {
+        this.ocean = ocean;
+    }
+    @Autowired
+    public void setNeedle(Needle7 needle){
+        this.needle = needle;
+    }
+}
